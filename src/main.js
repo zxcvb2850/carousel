11b6aa22e1567ef9aeb1dd079115998ed50db807
myPc <zxcvb2850@163.com>
2019-02-23 17:08:25 +0800

    添加资源文件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import "./common/styles/index.less"
-// import "./common/styles/css.css"
 
-const index = 1
-console.log("main.js", index)
-
-function abc() {
-  console.log(index)
-}
-
-abc()
+const container = document.querySelector(".swiper-container")
+console.log(container)
