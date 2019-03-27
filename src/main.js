7958782bb21a6fd5602b476b514d51436e055618
myPc <zxcvb2850@163.com>
2019-03-27 21:09:55 +0800

    :rocket: 提交完成移动端轮播

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import Swiper from "./components/Swiper"
 import "./main.less"
 
 const swiper = new Swiper(".swiper-container", {
-  loop: false,
-  autoPlay: false,
-  speed: 2000,
+  loop: true,
+  autoPlay: true,
+  speed: 3000,
   click: true,
   clickCallback: function (e, index) {
     console.log("当前点击的是：", index, e)
   },
   isArrow: true,
-  css: false,
+  css: true,
   isPaginat: true,
   swiperWrapClass: "swiper-wrapper",
   changeCurrIndex: function (index) {
